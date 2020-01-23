4bfc5e935bb994eb5863ab90aa95533145246dcc
fbuitron <franciscobuitron21@gmail.com>
2020-01-23 15:11:39 -0800

    rough password of x length generated, prompts

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+document.getElementById('generate').addEventListener('click', function () {
+  let passLength = prompt('How long do you want your password to be? (8-128 characters')
+  let passChar = prompt('What characters do you want in your password? (u for uppercase, l for lowercase, n for numeric, s for special characters)')
+  function generatePassword() {
+    let length = passLength,
+      charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+      retVal = "";
+    for (let i = 0, n = charset; i < length; i++) {
+      retVal += charset.charAt(Math.floor(Math.random) * n);
+    }
+    return retVal;
+  }
+  function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+  generateBtn.addEventListener("click", writePassword());
+})
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
