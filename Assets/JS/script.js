f7622763e79ed45f562592d7269716166b0b8c53
fbuitron <franciscobuitron21@gmail.com>
2020-01-23 23:03:31 -0800

    made button functional

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,26 +1,33 @@
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-document.getElementById('generate').addEventListener('click', function () {
-  let passLength = prompt('How long do you want your password to be? (8-128 characters')
-  let passChar = prompt('What characters do you want in your password? (u for uppercase, l for lowercase, n for numeric, s for special characters)')
-  function generatePassword() {
-    let length = passLength,
-      charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
-      retVal = "";
-    for (let i = 0, n = charset; i < length; i++) {
-      retVal += charset.charAt(Math.floor(Math.random) * n);
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '?', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+document.getElementById('generate').addEventListener('click', function generate() {
+  var user = prompt(`Do you want your password to contain lower, upper, numeric, or special characters? 
+Note that choosing an option to the right will include all options to the left of it.`)
+  var passLength = prompt('How long do you want your password to be? (8-128 characters)')
+  var randomLetter = "";
+  for (i = 0; i < passLength; i++) {
+    if (user === 'lower') {
+      randomLetter += lower[Math.floor(lower.length * Math.random())];
+    } else if (user === 'upper') {
+      randomLetter += upper[Math.floor(upper.length * Math.random())];
+    } else if (user === 'numeric') {
+      randomLetter += numeric[Math.floor(numeric.length * Math.random())];
+    } else if (user === 'special') {
+      randomLetter += special[Math.floor(special.length * Math.random())];
     }
-    return retVal;
   }
-  function writePassword() {
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
+  if (passLength < 8 || passLength > 128) {
+    alert('Please choose a password length between 8 and 128 characters')
+    return generate()
   }
-  generateBtn.addEventListener("click", writePassword());
+  document.getElementById("password").innerText = randomLetter;
 })
 
 // Write password to the #password input
